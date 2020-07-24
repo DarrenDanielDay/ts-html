97509bc3fd463fab7340be6f67f17ddc2a8ebe34
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2020-07-24 16:37:25 +0800

    Change back to template

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,11 @@
 <template>
   <div id="app">
-    <div>将鼠标移到框内查看3D立方体</div>
     <canvas width="400" height="300" id="cvs"></canvas>
-    <div>屏幕所在平面方程：x + y + z = 100√3</div>
-    <div>当前鼠标三维坐标</div>
-    <div id="message"></div>
-    <div>当前鼠标二维坐标</div>
-    <div id="position"></div>
-    <div id="plane"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import {
-  CoordinateSystem,
-  visualPlaneOf,
-  TestCuboid,
-  rotateBasis3D,
-  Point,
-  unitization,
-  dotOf,
-  Basis3D,
-  substractOn,
-  cloneOf,
-  addOn,
-  multiplyOn,
-  crossProductOf,
-  decomposition,
-  representationOf,
-  screenPositionOf,
-  toPrettyString
-} from "./utils/matrix3d";
-// const cvs = document.getElementById("cvs") as HTMLCanvasElement;
-const r2 = Math.sqrt(2);
-const r3 = Math.sqrt(3);
-const r6 = Math.sqrt(6);
-const stdBasis = {
-  xBasis: {
-    x: 1 / r2,
-    y: 1 / r6,
-    z: 1 / r3
-  },
-  yBasis: {
-    x: -1 / r2,
-    y: 1 / r6,
-    z: 1 / r3
-  },
-  zBasis: {
-    x: 0,
-    y: -r2 / r3,
-    z: 1 / r3
-  }
-};
-const coordinateSystem: CoordinateSystem = {
-  origin: {
-    x: 200,
-    y: 150,
-    z: -100
-  },
-  basis: stdBasis
-};
 @Component({
   components: {}
 })
@@ -71,27 +16,6 @@ export default class App extends Vue {
     this.canvas = document.getElementById("cvs") as any;
     this.context2d = this.canvas.getContext("2d") as any;
     this.draw();
-    this.canvas.addEventListener("mousemove", e => {
-      console.log("mousemove", e.offsetX, e.offsetY);
-      this.clear();
-      const x = e.offsetX;
-      const y = e.offsetY;
-      const ray = screenPositionOf({
-        origin: coordinateSystem.origin,
-        basis: stdBasis,
-      }, {x, y})
-      document.getElementById("message")!.innerText = toPrettyString(ray);
-      document.getElementById("position")!.innerText = toPrettyString({ x, y });
-      const xRay = representationOf(unitization(ray), stdBasis);
-      const yRay = unitization(crossProductOf(stdBasis.xBasis, xRay))
-      const zRay = unitization(crossProductOf(yRay, xRay));
-      coordinateSystem.basis = {
-        xBasis: xRay,
-        yBasis: yRay,
-        zBasis: zRay,
-      }
-      this.draw();
-    });
   }
   clear() {
     console.log("clear!");
@@ -101,8 +25,6 @@ export default class App extends Vue {
   draw() {
     console.log("draw!");
     // TODO
-    const plane = visualPlaneOf(coordinateSystem);
-    new TestCuboid(50, 50, 50, coordinateSystem).draw(this.context2d);
   }
 }
 </script>
@@ -118,15 +40,5 @@ export default class App extends Vue {
 }
 #cvs {
   border: black 1px solid;
-}
-.buttons {
-  display: flex;
-  justify-content: space-around;
-}
-.clear {
-  background: #cc4444;
-}
-.draw {
-  background: #44cc44;
 }
 </style>
