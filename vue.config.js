86b15df4af7c3475258b26934a5373fdc19b4e74
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-09 15:48:46 +0800

    change title

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@
  */
  const config = {
   outputDir: './dist/canvas-with-3d',
+  // pages: {
+  //   index: {
+  //     entry: 'src/main.ts',
+  //     title: '3D Cube',
+  //   }
+  // },
   publicPath: (process.env.DEPLOY_BASE_URL || "") + "/"
 };
 module.exports = config;
